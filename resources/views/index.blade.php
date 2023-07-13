ec80f607ce83c1cb8869f559c091b922ab4bb441
Muhammad Dhivara Aurian <64950057+betaindo45@users.noreply.github.com>
2023-07-13 15:41:22 +0700

    Update TailwindCSS config

---
+++ resources/views/index.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Permintaan Surat - Desa Semboro</title>
+    <link href="../css/output.css" rel="stylesheet">
+</head>
+<body>
+    <section id="home" class="pt-36">
+        <div class="container">
+            <div class="flex flex-wrap">
+                <div class="w-full self-center px-4">
+                    <h1 class="font-semibold text-primary text-4xl">Desa Semboro</h1>
+                    <h2 class="font-medium text-slate-800 text-lg mb-5">Kabupaten Jember</h2>
+                    <p class="font-medium text-slate-600 mb-10 leading-relaxed">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores sint perspiciatis possimus rem eius dolor ea veritatis ducimus et perferendis!</p>
+                    <a href="#" class="text-base font-semibold text-white bg-primary py-3 px-8 rounded-full hover:shadow-2xl hover:bg-hover hover:text-teal-950 transition duration-500 ease-in-out">Hubungi Kami!</a>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
