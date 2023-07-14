391be089cee61ae7f5611aaa104803f3c78f4845
GavinoBarcaDiyuNugroho <103358416+GavinoBarcaDiyuNugroho@users.noreply.github.com>
2023-07-14 16:57:22 +0700

    Pembuatan controller dan contents
    controller masih template untuk database sudah lengkap bisa di cek

---
+++ database/migrations/2023_07_14_094937_create_contents_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contents', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama', 200);
+            $table->string('nik', 50);
+            $table->string('kk',50);
+            $table->text('keperluan');
+            $table->text('keterangan');
+            $table->text('nohp');
+            $table->text('email')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contents');
+    }
+};
