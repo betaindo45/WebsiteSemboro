72a767a21facbcb0de9249e6541e38aaedb6e254
Muhammad Dhivara Aurian <64950057+betaindo45@users.noreply.github.com>
2023-07-28 17:27:30 +0700

    Adding Dynamic Form

--- resources/views/formsurat.blade.php
+++ resources/views/formsurat.blade.php
@@ -18,7 +18,8 @@
             <div class="grid min-h-screen place-items-center -bottom-10 p-0 m-12 max-md:m-0.5">
                 <div class="w-auto p-12 bg-white rounded-lg max-md:p-5">
                     <h1 class="text-xl font-semibold">Tolong isi informasi anda dibawah untuk melakukan pengajuan</h1>
-                    <form action="/registersurat" method="post" class="mt-6" enctype="multipart/form-data">
+                    <form action="/registersurat" method="post" class="mt-6" enctype="multipart/form-data"
+                        id="surat">
                         @csrf
                         <label for="name" class="block text-xs font-semibold text-gray-600 uppercase">Nama<span
                                 class="text-red-600 text-2xl">*</span></label>
@@ -28,7 +29,7 @@
                         @error('name')
                             <div class="alert alert-danger"> {{ $message }} </div>
                         @enderror
-                        
+
 
                         <label for="nik" class="block mt-2 text-xs font-semibold text-gray-600 uppercase">Nomor
                             Induk Kependudukan (NIK)<span class="text-red-600 text-2xl">*</span></label>
@@ -40,7 +41,8 @@
                         @enderror
 
 
-                        <label for="imagenik" class="block mt-2 text-xs font-semibold text-gray-600 uppercase">Upload Kartu Tanda Penduduk (KTP)<span class="text-red-600 text-2xl">*</span></label>
+                        <label for="imagenik" class="block mt-2 text-xs font-semibold text-gray-600 uppercase">Upload
+                            Kartu Tanda Penduduk (KTP)<span class="text-red-600 text-2xl">*</span></label>
                         <input id="imagenik" type="file" name="imagenik" placeholder=""
                             class="@error('nik') is-invalid @enderror block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner rounded-lg"
                             required value="{{ old('nik') }}" />
@@ -56,38 +58,52 @@
                         @enderror
 
 
-                        <label for="imagekk" class="block mt-2 text-xs font-semibold text-gray-600 uppercase">Upload Kartu Keluarga (KK)<span class="text-red-600 text-2xl">*</span></label>
+                        <label for="imagekk" class="block mt-2 text-xs font-semibold text-gray-600 uppercase">Upload
+                            Kartu Keluarga (KK)<span class="text-red-600 text-2xl">*</span></label>
                         <input id="imagekk" type="file" name="imagekk" placeholder=""
                             class="@error('nik') is-invalid @enderror block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner rounded-lg"
                             required value="{{ old('nik') }}" />
 
-                            
+
                         <label for="keperluan"
                             class="block mt-2 text-xs font-semibold text-gray-600 uppercase">Keperluan Surat<span
                                 class="text-red-600 text-2xl">*</span></label>
-                        <select name="keperluan" id="keperluan" class="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner rounded-lg" required>
+                        <select name="keperluan" id="keperluan"
+                            class="block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner rounded-lg"
+                            required>
                             <option value="" disabled selected hidden>Pilih Salah Satu</option>
-                            <option value="Surat Keterangan Kepemilikan Kendaraan Bermotor">Surat Keterangan Kepemilikan Kendaraan Bermotor</option>
+                            <option value="Surat Keterangan Kepemilikan Kendaraan Bermotor">Surat Keterangan Kepemilikan
+                                Kendaraan Bermotor</option>
                             <option value="Surat Keterangan Penghasilan">Surat Keterangan Penghasilan</option>
                             <option value="Surat Keterangan Miskin">Surat Keterangan Miskin</option>
                             <option value="Surat Keterangan Tidak Mampu">Surat Keterangan Tidak Mampu</option>
-                            <option value="Surat Keterangan Tidak Memiliki Rumah">Surat Keterangan Tidak Memiliki Rumah</option>
+                            <option value="Surat Keterangan Tidak Memiliki Rumah">Surat Keterangan Tidak Memiliki Rumah
+                            </option>
                             <option value="Surat Keterangan Beda Identitas">Surat Keterangan Beda Identitas</option>
-                            <option value="Surat Keterangan Organisasi Terlarang">Surat Keterangan Organisasi Terlarang (OT)</option>
+                            <option value="Surat Keterangan Organisasi Terlarang">Surat Keterangan Organisasi Terlarang
+                                (OT)</option>
                             <option value="Surat Keterangan Domisili Lembaga">Surat Keterangan Domisili Lembaga</option>
-                            <option value="Surat Keterangan Domisili/Tempat Tinggal">Surat Keterangan Domisili/Tempat Tinggal</option>
-                            <option value="Surat Keterangan Pindah Tempat Tinggal">Surat Keterangan Pindah Tempat Tinggal</option>
-                            <option value="Surat Keterangan Belum Pernah Nikah">Surat Keterangan Belum Pernah Nikah</option>
+                            <option value="Surat Keterangan Domisili/Tempat Tinggal">Surat Keterangan Domisili/Tempat
+                                Tinggal</option>
+                            <option value="Surat Keterangan Pindah Tempat Tinggal">Surat Keterangan Pindah Tempat
+                                Tinggal</option>
+                            <option value="Surat Keterangan Belum Pernah Nikah">Surat Keterangan Belum Pernah Nikah
+                            </option>
                             <option value="Surat Keterangan Janda-Duda">Surat Keterangan Janda Duda</option>
                             <option value="Surat Keterangan Kenal Lahir">Surat Keterangan Kenal Lahir</option>
                             <option value="Surat Keterangan Kematian">Surat Keterangan Kematian</option>
                             <option value="Surat Keterangan Usaha">Surat Keterangan Usaha</option>
-                            <option value="Surat Keterangan Kepemilikan Tanah">Surat Keterangan Kepemilikan Tanah</option>
+                            <option value="Surat Keterangan Kepemilikan Tanah">Surat Keterangan Kepemilikan Tanah
+                            </option>
                             <option value="Surat Keterangan Lain-Lain (Umum)">Surat Keterangan Lain-Lain (Umum)</option>
                         </select>
                         @error('keperluan')
                             <div class="alert alert-danger"> {{ $message }} </div>
                         @enderror
+
+                        <div id="dynamic-form">
+
+                        </div>
 
 
                         <label for="keterangan"
@@ -123,4 +139,79 @@
                 </div>
             </div>
     </section>
+    <script>
+        const selectOption = document.getElementById("keperluan");
+        const dynamicInputContainer = document.getElementById("dynamic-form");
+        const dynamicForm = document.getElementById("surat");
+
+        selectOption.addEventListener("change", handleOptionChange);
+
+        function handleOptionChange() {
+            // Clear previous dynamic input fields
+            dynamicInputContainer.innerHTML = '';
+
+            // Check the selected option and create dynamic input field accordingly
+            if (selectOption.value === 'Surat Keterangan Pindah Tempat Tinggal') {
+                createRequiredInput('alamatTujuan', 'Alamat Tujuan');
+            }
+        }
+
+        function createRequiredInput(inputName, label) {
+            const inputElement = document.createElement('input');
+            inputElement.type = 'text';
+            inputElement.name = 'alamattujuan';
+            inputElement.id = 'alamaattujuan';
+            inputElement.className =
+                'block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner rounded-lg';
+            inputElement.placeholder = label;
+            inputElement.required = true;
+            inputElement.value = "{{ old('alamattujuan') }}";
+
+            const inputElement2 = document.createElement('input');
+            inputElement2.type = 'string';
+            inputElement2.name = 'desa';
+            inputElement2.id = 'desa';
+            inputElement2.className =
+                'block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner rounded-lg';
+            inputElement2.placeholder = 'Desa/Kelurahan';
+            inputElement2.required = true;
+            inputElement2.value = "{{ old('desa') }}";
+
+            const inputElement3 = document.createElement('input');
+            inputElement3.type = 'string';
+            inputElement3.name = 'kecamatan';
+            inputElement3.id = 'kecamatan';
+            inputElement3.className =
+                'block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner rounded-lg';
+            inputElement3.placeholder = 'Kecamatan';
+            inputElement3.required = true;
+            inputElement3.value = "{{ old('kecamatan') }}";
+
+            const inputElement4 = document.createElement('input');
+            inputElement4.type = 'string';
+            inputElement4.name = 'kota';
+            inputElement4.id = 'kota';
+            inputElement4.className =
+                'block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner rounded-lg';
+            inputElement4.placeholder = 'Kota/Kabupaten';
+            inputElement4.required = true;
+            inputElement4.value = "{{ old('kota') }}";
+
+            const inputElement5 = document.createElement('input');
+            inputElement5.type = 'text';
+            inputElement5.name = 'pengikut';
+            inputElement5.id = 'Pengikut';
+            inputElement5.className =
+                'block w-full p-3 mt-2 text-gray-700 bg-gray-200 appearance-none focus:outline-none focus:bg-gray-300 focus:shadow-inner rounded-lg';
+            inputElement5.placeholder = 'Pengikut';
+            inputElement5.required = true;
+            inputElement5.value = "{{ old('pengikut') }}";
+
+            dynamicInputContainer.appendChild(inputElement);
+            dynamicInputContainer.appendChild(inputElement2);
+            dynamicInputContainer.appendChild(inputElement3);
+            dynamicInputContainer.appendChild(inputElement4);
+            dynamicInputContainer.appendChild(inputElement5);
+        }
+    </script>
 </body>
