187ffc5c170b72c712d4da3e591c9f34d72065e1
Muhammad Dhivara Aurian <64950057+betaindo45@users.noreply.github.com>
2023-07-31 20:48:10 +0700

    Update Profile and Footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,17 +1,17 @@
 <footer class="bg-secondary pt-12 pb-12">
     <div class="container">
         <div class="flex flex-wrap">
-            <div class="w-full px-4 mb-6 text-slate-600 font-medium md:w-1/3 text-lg">
-                <h2 class="font-bold text-4xl mb-5 text-white">Balai Desa Semboro</h2>
-                <h3>pemdessemboro@gmail.com</h3>
-                <p>Jl. Melati No. 1</p>
+            <div class="w-full px-4 mb-6 text-slate-600 font-medium md:w-1/3 text-lg max-lg:text-base">
+                <h2 class="font-bold text-4xl max-lg:text-2xl mb-5 text-white">Balai Desa Semboro</h2>
+                <a href="mailto: pemdessemboro@gmail.com"">pemdessemboro@gmail.com</a><br>
+                <a href="https://goo.gl/maps/TeXh27UbojJkh4UC7" target="_blank">Jl. Melati No.1</a>
                 <p>Kecamatan Semboro</p>
                 <p>Kabupaten Jember</p>
                 <p>68157</p>
             </div>
-            <div class="w-full px-4 mb-6 text-slate-600 font-medium md:w-1/3 text-center">
-                <h3 class="font-bold text-4xl mb-5 text-white">Pengembang:</h3>
-                <ul class="inline-block text-lg font-semibold">
+            <div class="w-full px-4 mb-6 text-slate-600 font-medium md:w-1/3 text-center max-md:text-left">
+                <h3 class="font-bold text-4xl mb-5 text-white max-lg:text-2xl">Pengembang:</h3>
+                <ul class="inline-block text-lg font-semibold max-lg:text-base">
                     <li>
                         <p class="mb-3">Gavino Barca Diyu Nugroho</p>
                     </li>
@@ -19,10 +19,10 @@
                         <p class="mb-3">Muhammad Dhivara Aurian</p>
                     </li>
                 </ul>
-            </div>
-            <div class="w-full px-4 mb-6 text-slate-600 font-medium md:w-1/3 text-right">
-                <h3 class="font-bold text-4xl mb-5 text-white">Fitur Web:</h3>
-                <ul class="inline-block text-lg font-semibold">
+            </div>  
+            <div class="w-full px-4 mb-6 text-slate-600 font-medium md:w-1/3 text-right max-md:text-left">
+                <h3 class="font-bold text-4xl mb-5 text-white max-lg:text-2xl">Fitur Web:</h3>
+                <ul class="inline-block text-lg font-semibold max-lg:text-base">
                     <li>
                         <a href="/" class="mb-3 hover:text-slate-400">Beranda</a>
                     </li>
@@ -39,7 +39,8 @@
                     <img src="{{ asset('img/logommd.png') }}" alt="" class="h-auto m-5" style="width: 100px">
                     <img src="{{ asset('img/logoub.png') }}" alt="" class="h-auto m-5" style="width: 100px">
                 </div>
-                <h3>Dibuat Sebagai Salah Satu Program Mahasiswa Membangun Desa</h3>
+                <h3>Dibuat Sebagai Salah Satu Program Kerja</h3>
+                <h3>Mahasiswa Membangun Desa</h3>
                 <h3>Universitas Brawijaya</h3>
                 <h3>2023</h3>
             </div>
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -7,40 +7,128 @@
     @else
         <x-header />
     @endauth
-    <section id="profile" class="">
+
+    <div id="profil">
         <div class="container">
-            <div class="flex flex-wrap justify-center h-screen">
-                <section id="profil">
-                    <div class="container">
-                        <div class="w-full">
-                            <div class="mx-auto text-center bg-slate-500/30 rounded-md backdrop-blur-lg">
-                                <div class="w-auto p-10 mt-24">
-                                    <h2 class="font-semibold text-3xl text-primary max-md:text-xl max-sm:text-sm mb-10">
-                                        Profil Desa Semboro
-                                    </h2>
-                                    <p class="text-white text-lg max-md:text-base max-sm:text-sm text-left">&emsp; Desa
-                                        Semboro merupakan
-                                        salah satu dari 6 desa yang terletak wilayah administrasi Kecamatan Semboro
-                                        Kabupaten Jember. Sejarah desa Semboro tidak terlepas dari sejarah masyarakat
-                                        campuran antara Jawa dan Madura di Kabupaten Jember. Sepanjang jalan di desa ini
-                                        banyak ditumbuhi pohon asem di kanan kirinya. Di desa ini awalnya banyak orang
-                                        pendatang yang dikenal sebagai boro (dalam bahasa jawa). Sehingga jadilah nama
-                                        Semboro yang berasal dari gabungan dua kata, yaitu asem dan boro.</p>
-                                    <p class="text-white text-lg max-md:text-base max-sm:text-sm mt-5 text-left">&emsp;
-                                        Wilayah Desa Semboro berada di dataran rendah dan memiliki sungai yang membuat
-                                        Desa Semboro memiliki potensi untuk hasil panen yang melimpah. Selain itu
-                                        banyaknya pendudukn dan beragamnya budaya yang ada di dalam wilayah desa
-                                        menjadikannya sebagai sebuah aset pembangunan yang potensial. Banyaknya penduduk
-                                        dengan angkatan kerja di berbagai bidang lapangan dan tingginya potensi
-                                        kepemudaan juga membuat Desa Semboro memiliki sumber daya manusia yang cukup
-                                        memadai untuk menggerakan ekonomi.</p>
-                                </div>
-                            </div>
+            <div class="flex flex-wrap justify-center">
+                {{-- Profil --}}
+                <div
+                    class="w-auto my-24 p-10 bg-slate-500/30 rounded-lg backdrop-blur-lg flex flex-wrap justify-between items-center max-md:p-5">
+                    <div class="w-full md:w-1/2 flex justify-center items-center px-5">
+                        <div class="text-center">
+                            <h2 class="font-semibold text-3xl text-primary max-md:text-xl max-sm:text-lg mb-10">
+                                Profil Desa Semboro
+                            </h2>
+                            <p class="text-white text-lg max-md:text-base max-sm:text-sm text-left">&emsp; Desa
+                                Semboro merupakan
+                                salah satu dari 6 desa yang terletak wilayah administrasi Kecamatan Semboro
+                                Kabupaten Jember. Sejarah desa Semboro tidak terlepas dari sejarah masyarakat
+                                campuran antara Jawa dan Madura di Kabupaten Jember. Sepanjang jalan di desa ini
+                                banyak ditumbuhi pohon asem di kanan kirinya. Di desa ini awalnya banyak orang
+                                pendatang yang dikenal sebagai boro (dalam bahasa jawa). Sehingga jadilah nama
+                                Semboro yang berasal dari gabungan dua kata, yaitu asem dan boro.</p>
+                            <p class="text-white text-lg max-md:text-base max-sm:text-sm mt-5 text-left">&emsp;
+                                Wilayah Desa Semboro berada di dataran rendah dan memiliki sungai yang membuat
+                                Desa Semboro memiliki potensi untuk hasil panen yang melimpah. Selain itu
+                                banyaknya pendudukn dan beragamnya budaya yang ada di dalam wilayah desa
+                                menjadikannya sebagai sebuah aset pembangunan yang potensial. Banyaknya penduduk
+                                dengan angkatan kerja di berbagai bidang lapangan dan tingginya potensi
+                                kepemudaan juga membuat Desa Semboro memiliki sumber daya manusia yang cukup
+                                memadai untuk menggerakan ekonomi.</p>
                         </div>
                     </div>
-                </section>
+                    <div class="w-full md:w-1/2 flex justify-center items-center px-5">
+                        <iframe class="rounded-lg"
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d38955.781847715996!2d113.42090091383017!3d-8.197217450062233!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd68bd7f36340d7%3A0xb29c4e09cefc0694!2sSemboro%2C%20Jember%20Regency%2C%20East%20Java!5e1!3m2!1sen!2sid!4v1690795166329!5m2!1sen!2sid"
+                            width="550" height="550" style="border:0;" allowfullscreen="" loading="lazy"
+                            referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                </div>
+                {{-- Visi Misi --}}
+                <div class="w-auto mt-0 mb-12 p-5 bg-slate-500/30 rounded-lg backdrop-blur-lg flex flex-wrap">
+                    <div class="w-full">
+                        <h3 class="font-semibold text-4xl text-primary max-md:text-xl max-sm:text-lg mb-10 text-center">
+                            Visi dan Misi</h3>
+                    </div>
+                    <div class="w-full md:w-1/2 mt-5">
+                        <h3 class="font-semibold text-2xl text-primary max-md:text-xl max-sm:text-lg mb-10">Visi</h3>
+                        <p class="text-white font-lg max-md:text-base">Visi Desa Semboro adalah " terbangunnya tata
+                            kelola pemerintahan desa yang baik dan bersih guna mewujudkan kehidupan masyarakat desa yang
+                            adil, makmur dan sejahtera"</p>
+                    </div>
+                    <div class="w-full md:w-1/2 mt-5">
+                        <h3 class="font-semibold text-2xl text-primary max-md:text-xl max-sm:text-lg">Misi</h3>
+                        <ol class="text-white font-lg max-md:text-base">
+                            <li>
+                                <p>1. Melakukan sistem kinerja aparatur pemerintahan desa guna meningkatkan kwalitas
+                                    pelayanan kepada masyarakat.</p>
+                            </li>
+                            <li>
+                                <p>2. Menyelenggarakan pemerintahan yang bersih, terbebas dari korupsi, serta
+                                    bentuk-bentuk penyelewengan lainnya.</p>
+                            </li>
+                            <li>
+                                <p>3. Menyelenggarakan urusan pemerintahan desa secara terbuka dan bertanggung jawab
+                                    sesuai dengan peraturan perundang-undangan.</p>
+                            </li>
+                            <li>
+                                <p>4. Meningkatkan perekonomian masyarakat melalui pendampingan berupa penyuluhan khusus
+                                    pada UKM, Wiraswasta dan petani.</p>
+                            </li>
+                            <li>
+                                <p>5. Meningkatkan pembangunan lingkungan pedesaan dan mutu kesejahteraan masyarakat
+                                    untuk mencapai taraf kehidupan yang lebih baik dan layak sehingga menjadi desa yang
+                                    maju dan mandiri.</p>
+                            </li>
+                        </ol>
+                    </div>
+                </div>
+                {{-- Explore --}}
+                <div
+                    class="w-auto mt-0 mb-12 p-5 bg-slate-500/30 rounded-lg backdrop-blur-lg flex flex-wrap justify-center">
+                    <div class="w-full">
+                        <h3 class="font-semibold text-3xl text-primary max-md:text-xl max-sm:text-lg mb-10 text-center">
+                            Explore Semboro</h3>
+                    </div>
+                    <div class="w-full m-5 p-5 bg-white text-center rounded-lg md:w-1/3 overflow-hidden">
+                        <img src="{{ asset('img/pasarsemboro.jpg') }}" alt="" class="rounded-lg" width="400"
+                            style="object-fit: cover;">
+                        <h2 class="text-base md:text-2xl">Pasar Semboro</h2>
+                        <p class="text-left mt-5">Pasar Semboro dikenal
+                            sebagai pusat perdagangan dan kegiatan ekonomi yang ramai, serta menawarkan
+                            berbagai pengalaman berbelanja yang menarik bagi para pengunjungnya.</p>
+                    </div>
+                    <div class="w-full m-5 p-5 bg-white text-center rounded-lg md:w-1/2 lg:w-1/3">
+                        <img src="{{ asset('img/tugusemboro.jpeg') }}" alt="" class="rounded-lg h-300 w-auto">
+                        <h2 class="text-base md:text-2xl">Tugu Semboro</h2>
+                        <p class="text-left mt-5">Tugu ini berada di pinggir Pasar Semboro berbatasan dengan Desa
+                            Sidomekar. Tugu ini menjadi
+                            titik menarik yang patut dikunjungi. Selain sebagai objek fotografi yang indah, tugu ini
+                            juga memberikan kesempatan untuk merenung tentang arti dan makna dari monumen ini dalam
+                            konteks kehidupan masyarakat desa.</p>
+                    </div>
+                    <div class="w-full m-5 p-5 bg-white text-center rounded-lg md:w-1/2 lg:w-1/3">
+                        <img src="{{ asset('img/lapangan.png') }}" alt="" class="rounded-lg h-300 w-auto">
+                        <h2 class="text-base md:text-2xl">Lapangan Semboro</h2>
+                        <p class="text-left mt-5">Lapangan Semboro berada di sebelah Balai Desa Semboro, lapangan ini
+                            sangatlah luas dan biasa digunakan oleh warga setempat untuk melakukan berbagai aktivitas
+                            mulai dari jalan santai, berolahraga, maupun untuk latihan berkendara.</p>
+                    </div>
+                    <div class="w-full m-5 p-5 bg-white text-center rounded-lg md:w-1/2 lg:w-1/3">
+                        <img src="{{ asset('img/pgsemboro.jpeg') }}" alt="" class="rounded-lg h-300 w-auto"
+                            style="object-fit: cover" width="400px">
+                        <h2 class="text-base md:text-2xl">PG Semboro</h2>
+                        <p class="text-left mt-5">PG Semboro adalah sebuah pabrik gula yang luar biasa dan menjadi
+                            kebanggaan Kabupaten Jember, Jawa Timur. PG (Pabrik Gula) ini merupakan salah satu pabrik
+                            gula terkemuka di wilayah tersebut dan telah memainkan peran penting dalam ekonomi dan
+                            pertanian daerah selama bertahun-tahun. Selain kontribusinya terhadap ekonomi lokal, PG
+                            Semboro juga berperan dalam meningkatkan kesejahteraan petani tebu di sekitar wilayahnya.
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
-    </section>
+    </div>
+
     <x-footer />
 </body>
